cf927d2f30070e101c768c82451b10bd4975634c
flexyzwork <flexyzwork@gmail.com>
2025-03-09 18:40:34 +0900

    [FEAT] transaction, userCourseProgress

--- apps/api/src/routes/transactionRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var transactionController_1 = require("../controllers/transactionController");
-var router = express_1.default.Router();
-router.get("/", transactionController_1.listTransactions);
-router.post("/", transactionController_1.createTransaction);
-router.post("/stripe/payment-intent", transactionController_1.createStripePaymentIntent);
-exports.default = router;
